bd6bd0dae278795c91bd9b83036f80ba48d85e6e
Kerlin Michel <michelkerlin@gmail.com>
2017-12-05 23:18:59 -0500

    Agent using model to determine move

--- player.py
+++ player.py
@@ -1,5 +1,9 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from sys import argv
+from queue import Queue
+from train_player import train_model, card_norm
+import numpy as np
+import operator
 
 sock = socket(AF_INET, SOCK_STREAM)
 
@@ -9,13 +13,18 @@ sock.send(b'VERSION:2.0.0\r\n')
 
 def parse_line(line):
     params = line.split(b':')
-    print('params', params)
     return int(params[1]), params[2], params[3].decode(), params[-1].decode().strip()
 
 def isShowDown(cards):
     return len([c for c in cards.split('|') if c]) == 3
 
 i = 0
+h = 1
+p1_bluff = 0
+p2_bluff = 0
+p1_rbluff = Queue(10)
+p2_rbluff = Queue(10)
+model = train_model()
 while True:
     line = sock.recv(4096)
     if not line:
@@ -23,13 +32,54 @@ while True:
         break
     line = line.split()[-1]
     params = parse_line(line)
+    if p1_rbluff.full():
+        p1_rbluff.get()
+    if p2_rbluff.full():
+        p2_rbluff.get()
     if isShowDown(params[3]) or not line:
+        plays = list(params[2])
+        if len(plays) < 3:
+            i = 0
+            continue
+        cards = params[3].split('|')
+        del cards[params[0]]
+        p1i = (params[0] + 1) % 2
+        if cards[0] == 'Js':
+            if plays[p1i] == 'c':
+                p1_bluff += 0.75
+            elif plays[p1i] == 'r':
+                p1_bluff += 1
+        if cards[0] == 'Qs':
+            if plays[p1i] == 'c':
+                p1_bluff += 0.5
+            elif plays[p1i] == 'r':
+                p1_bluff += 0.75
+        if cards[0] == 'Ks':
+            if plays[p1i] == 'c':
+                p1_bluff += 0.125
+            if plays[p1i] == 'r':
+                p1_bluff += 0.25
+        p1_bluff += p1_bluff
+        p1_rbluff.put(p1_bluff)
         i = 0
         continue
-    if i % 3 == params[0]:
+    #print('params', params)
+    if i % 3 == params[0] or True:
+       m = 0
        if 'r' in params[2]:
-           move = 'c'
+           moves = ['f', 'r']
        else:
-           move = 'c'
-       sock.send(line + b':' + move.encode() + b'\r\n')
+           moves = ['c', 'r']
+       mycard = params[3].split('|')[0]
+       if mycard:
+           p1rb = sum(list(p1_rbluff.queue))/10
+           p2rb = sum(list(p2_rbluff.queue))/10
+           X = np.append(card_norm[mycard], [p1rb, p1_bluff/h, p2rb, p2_bluff/h])
+           pred = model.predict(np.asarray([X])) 
+           #print(pred[0].tolist()[0], pred[1].tolist()[0])
+           index, value = max(enumerate(pred[0].tolist()[0]), key=operator.itemgetter(1))
+           if card_norm[mycard].tolist().index(1) < index:
+               me = 1 
+       sock.send(line + b':' + moves[1].encode() + b'\r\n')
     i += 1
+    h += 1
--- train_player.py
+++ train_player.py
@@ -18,75 +18,77 @@ card_norm = {
 }
 
 def get_data():
-    #for f in listdir('./play_dataset'):
-    #    pass
-    f = listdir('./play_dataset')[0]
-    
-    with open('./play_dataset/' + f) as log:
-        data = [l.strip() for l in log.readlines()[4:]]
-        me, p1, p2 = data[0].split(':')[5].split('|')
-        p1_bluffs = 0
-        p2_bluffs = 0
-        p1_recent_bluffs = Queue(10)
-        p2_recent_bluffs = Queue(10)
-        X = []
-        y = []
-        i = 1
-        for l in data[:-1]:
-            params = l.split(':')
-            cards = params[3].split('|')
-            order = params[5].split('|')
-            plays = list(params[2])
-            me_c = cards[order.index(me)]
-            p1_i = order.index(p1)
-            p2_i = order.index(p2)
-            p1_bluff = 0
-            p2_bluff = 0
-            if cards[p1_i] == 'Js':
-                if plays[p1_i] == 'c':
-                    p1_bluff += 0.75
-                elif plays[p1_i] == 'r':
-                    p1_bluff += 1
-            if cards[p1_i] == 'Qs':
-                if plays[p1_i] == 'c':
-                    p1_bluff += 0.5
-                elif plays[p1_i] == 'r':
-                    p1_bluff += 0.75
-            if cards[p1_i] == 'Ks':
-                if plays[p1_i] == 'c':
-                    p1_bluff += 0.125
-                if plays[p1_i] == 'r':
-                    p1_bluff += 0.25
+    num = 0
+    for f in listdir('./play_dataset'):
+        num += 1
+        if num > 10:
+            break
+    #f = listdir('./play_dataset')[0]
+        with open('./play_dataset/' + f) as log:
+            data = [l.strip() for l in log.readlines()[4:]]
+            me, p1, p2 = data[0].split(':')[5].split('|')
+            p1_bluffs = 0
+            p2_bluffs = 0
+            p1_recent_bluffs = Queue(10)
+            p2_recent_bluffs = Queue(10)
+            X = []
+            y = []
+            i = 1
+            for l in data[:-1]:
+                params = l.split(':')
+                cards = params[3].split('|')
+                order = params[5].split('|')
+                plays = list(params[2])
+                me_c = cards[order.index(me)]
+                p1_i = order.index(p1)
+                p2_i = order.index(p2)
+                p1_bluff = 0
+                p2_bluff = 0
+                if cards[p1_i] == 'Js':
+                    if plays[p1_i] == 'c':
+                        p1_bluff += 0.75
+                    elif plays[p1_i] == 'r':
+                        p1_bluff += 1
+                if cards[p1_i] == 'Qs':
+                    if plays[p1_i] == 'c':
+                        p1_bluff += 0.5
+                    elif plays[p1_i] == 'r':
+                        p1_bluff += 0.75
+                if cards[p1_i] == 'Ks':
+                    if plays[p1_i] == 'c':
+                        p1_bluff += 0.125
+                    if plays[p1_i] == 'r':
+                        p1_bluff += 0.25
 
-            if cards[p2_i] == 'Js':
-                if plays[p2_i] == 'c':
-                    p2_bluff += 0.75
-                elif plays[p2_i] == 'r':
-                    p2_bluff += 1
-            if cards[p2_i] == 'Qs':
-                if plays[p2_i] == 'c':
-                    p2_bluff += 0.5
-                elif plays[p2_i] == 'r':
-                    p2_bluff += 0.75
-            if cards[p2_i] == 'Ks':
-                if plays[p2_i] == 'c':
-                    p2_bluff += 0.125
-                if plays[p2_i] == 'r':
-                    p2_bluff += 0.25
+                if cards[p2_i] == 'Js':
+                    if plays[p2_i] == 'c':
+                        p2_bluff += 0.75
+                    elif plays[p2_i] == 'r':
+                        p2_bluff += 1
+                if cards[p2_i] == 'Qs':
+                    if plays[p2_i] == 'c':
+                        p2_bluff += 0.5
+                    elif plays[p2_i] == 'r':
+                        p2_bluff += 0.75
+                if cards[p2_i] == 'Ks':
+                    if plays[p2_i] == 'c':
+                        p2_bluff += 0.125
+                    if plays[p2_i] == 'r':
+                        p2_bluff += 0.25
 
-            p1_bluffs += p1_bluff
-            p2_bluffs += p2_bluff
-            p1_recent_bluffs.put(p1_bluff)
-            p2_recent_bluffs.put(p2_bluff)
-            if i > 9:
-                p1_recent_bluffs.get()
-                p2_recent_bluffs.get()
-            p1_recent_bluff_rate = sum(list(p1_recent_bluffs.queue))/10
-            p2_recent_bluff_rate = sum(list(p2_recent_bluffs.queue))/10
-            #print(np.append(card_norm[me_c], (p1_recent_bluff_rate, p2_recent_bluff_rate, p1_bluffs/i, p2_bluffs/i)).tolist())
-            X.append(np.append(card_norm[me_c], (p1_recent_bluff_rate, p2_recent_bluff_rate, p1_bluffs/i, p2_bluffs/i)).tolist())
-            y.append((card_norm[cards[p1_i]], card_norm[cards[p2_i]]))
-            i += 1
+                p1_bluffs += p1_bluff
+                p2_bluffs += p2_bluff
+                p1_recent_bluffs.put(p1_bluff)
+                p2_recent_bluffs.put(p2_bluff)
+                if i > 9:
+                    p1_recent_bluffs.get()
+                    p2_recent_bluffs.get()
+                p1_recent_bluff_rate = sum(list(p1_recent_bluffs.queue))/10
+                p2_recent_bluff_rate = sum(list(p2_recent_bluffs.queue))/10
+                #print(np.append(card_norm[me_c], (p1_recent_bluff_rate, p2_recent_bluff_rate, p1_bluffs/i, p2_bluffs/i)).tolist())
+                X.append(np.append(card_norm[me_c], (p1_recent_bluff_rate, p2_recent_bluff_rate, p1_bluffs/i, p2_bluffs/i)).tolist())
+                y.append((card_norm[cards[p1_i]], card_norm[cards[p2_i]]))
+                i += 1
     return X, y
 
 def train_model():
@@ -126,11 +128,10 @@ def train_model():
     #model.add(Rounder())
     model = Model(inputs=input_layer, outputs=[output1, output2])
     model.compile(optimizer='adam', loss=['categorical_crossentropy', 'categorical_crossentropy'], metrics=['accuracy'])
-    history = model.fit(x=X_train, y=[y1_train, y2_train], batch_size=32, epochs=350, verbose=1)
-    print(model.evaluate(x=X_test, y=[y1_test, y2_test]))
-    [predictions1, predictions2] = model.predict(np.asarray(X_train[:10]))
-    for i,prediction in enumerate(predictions1):
-        print(X_test[i][:4], prediction, y1_test[i], predictions2[i], y2_test[i])
-
-train_model()
+    history = model.fit(x=X_train, y=[y1_train, y2_train], batch_size=32, epochs=50, verbose=1)
+    return model
+    #print(model.evaluate(x=X_test, y=[y1_test, y2_test]))
+    #[predictions1, predictions2] = model.predict(np.asarray(X_train[:10]))
+    #for i,prediction in enumerate(predictions1):
+    #    print(X_test[i][:4], prediction, y1_test[i], predictions2[i], y2_test[i])
 
